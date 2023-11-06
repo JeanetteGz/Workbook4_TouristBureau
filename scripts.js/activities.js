3c17da37f0190a6a570fd34132f6c505bc3460c8
Jeanettegz <Jeanetteglz99@gmail.com>
2023-11-06 16:13:54 -0500

    fixed some indentations

--- scripts.js/activities.js
+++ scripts.js/activities.js
@@ -1,171 +1,201 @@
-"use strict"
+"use strict";
 let selectCategoriesEl = document.getElementById("select-categories");
 let purchaseForm = document.getElementById("ticketForm");
 
 window.onload = function () {
     populateCategoryDropdown();
-    selectCategoriesEl.onchange = function(){
+    selectCategoriesEl.onchange = function () {
+        let activities = [
+            {
+                category: "Adventures",
+                id: "A101",
+                name: "Valley Hot Air Balloons",
+                description:
+                    "Enjoy a lovely hot air balloon ride over the valley at sunrise.  Call 800-555-1212 to reserve a date/time after you complete your e-ticket purchase.",
+                location: "121 S. Main Street",
+                price: 265.0,
+            },
+            {
+                category: "Adventures",
+                id: "A102",
+                name: "River Runners: Float Trip",
+                description:
+                    "A mellow float trip with lovely scenery, great fishing, just a few riffles, and it finishes back at our base. It is a perfect trip for those wishing to take their time, or those on a limited schedule.",
+                location: "145 FM 103",
+                price: 65.0,
+            },
+            {
+                category: "Adventures",
+                id: "A103",
+                name: "River Runners: Ride the Rapids",
+                description:
+                    "Experience 3-4 hours of Class II and III whitewater rafting with breathtaking scenery. It is a fun, thrilling, and memorable adventure that everyone from ages 8 and up can enjoy – no experience necessary!",
+                location: "145 FM 103",
+                price: 145.0,
+            },
+            {
+                category: "Arts & Crafts",
+                id: "AC101",
+                name: "Painting with a Twist : Oils",
+                description:
+                    "Enjoy 2 hours of creating an oil painting by following along with an experienced artist.  Drinks and snacks are included.",
+                location: "1991 Paint Drive",
+                price: 40.0,
+            },
+            {
+                category: "Arts & Crafts",
+                id: "AC102",
+                name: "Painting with a Twist : Watercolor",
+                description:
+                    "Enjoy 2 hours of creating a watercolor painting by following along with an experienced artist.  Drinks and snacks are included.",
+                location: "1991 Paint Drive",
+                price: 40.0,
+            },
+            {
+                category: "Museums",
+                id: "M101",
+                name: "Bravings Airship Museum",
+                description:
+                    "Enjoy climbing on and in our collection of small airplanes.  You will find bi-planes, experimental planes and small jets.",
+                location: "101 Airfield Drive",
+                price: 10.0,
+            },
+            {
+                category: "Museums",
+                id: "M102",
+                name: "Forks and Spoons Museum",
+                description:
+                    "Enjoy touring our qwerky Forks and Spoons Museum.  It houses the worlds largest collection of, you guessed it, forks and spoons!",
+                location: "1056 Lost Knives Court",
+                price: 3.0,
+            },
+            {
+                category: "Museums",
+                id: "M103",
+                name: "Steenges Computing Museum",
+                description:
+                    "Enjoy our the Stengees Computing Museum that illustrates how computing has changed over the last 60 years.",
+                location: "103 Technology Way",
+                price: 0.0,
+            },
+            {
+                category: "Wine Tastings",
+                id: "WT-101",
+                name: "Hastings Winery Tours and Tastings",
+                description:
+                    "Hastings Winery is a small, family owned winery in the heart of San Jose, CA. We pride ourselves on producing single vineyard, small-batch, handcrafted premium wines sourced from the finest grapes in our valley.",
+                location: "10987 FM 1187",
+                price: 12.0,
+            },
+            {
+                category: "Wine Tastings",
+                id: "WT-102",
+                name: "Lone Oak Winery",
+                description:
+                    "We are a family and friend centered winery that thrives to make each of our guests feel right at home. With a growing wine list of the finest local wines, we offer tours and an incredible experience. We are open for to-go, curbside, and delivery.",
+                location: "121 Burleson Court",
+                price: 0.0,
+            },
+            {
+                category: "Other",
+                id: "OTH101",
+                name: "Fire Department: Ride Along",
+                description:
+                    "Spend the day hanging out at one of our local fire stations, visiting with the staff and learning about their jobs.  If they receive a call, you can ride along with them!",
+                location: "10 Redline Drive",
+                price: 25.0,
+            },
+            {
+                category: "Other",
+                id: "OTH102",
+                name: "Homes For Our Neighbors",
+                description:
+                    "Yes, you are a visitor!  But what better way to learn about a community than volunteer with the locals to build affordable housing.  Whether it be for an hour or a week, we would love to have you with us!",
+                location: "Call (555) 555-5555",
+                price: 0.0,
+            },
+        ];
+        // Dropdown for the activities based on the category selected
+        let selectActivitiesEl = document.getElementById("select-activities");
+        selectActivitiesEl.style.visibility = "visible";
+        selectActivitiesEl.options.length = 0;
 
-let activities = [
-    {
-        category: "Adventures", 
-        id: "A101", 
-        name: "Valley Hot Air Balloons", 
-        description: "Enjoy a lovely hot air balloon ride over the valley at sunrise.  Call 800-555-1212 to reserve a date/time after you complete your e-ticket purchase.", 
-        location: "121 S. Main Street",
-        price: 265.00 
-    },
-    {
-        category: "Adventures", 
-        id: "A102", 
-        name: "River Runners: Float Trip", 
-        description: "A mellow float trip with lovely scenery, great fishing, just a few riffles, and it finishes back at our base. It is a perfect trip for those wishing to take their time, or those on a limited schedule.", 
-        location: "145 FM 103",
-        price: 65.00 
-    },
-    {
-        category: "Adventures", 
-        id: "A103", 
-        name: "River Runners: Ride the Rapids", 
-        description: "Experience 3-4 hours of Class II and III whitewater rafting with breathtaking scenery. It is a fun, thrilling, and memorable adventure that everyone from ages 8 and up can enjoy – no experience necessary!", 
-        location: "145 FM 103",
-        price: 145.00 
-    },     
-    {
-        category: "Arts & Crafts", 
-        id: "AC101", 
-        name: "Painting with a Twist : Oils", 
-        description: "Enjoy 2 hours of creating an oil painting by following along with an experienced artist.  Drinks and snacks are included.", 
-        location: "1991 Paint Drive",
-        price: 40.00 
-    },
-    {
-        category: "Arts & Crafts", 
-        id: "AC102", 
-        name: "Painting with a Twist : Watercolor", 
-        description: "Enjoy 2 hours of creating a watercolor painting by following along with an experienced artist.  Drinks and snacks are included.", 
-        location: "1991 Paint Drive",
-        price: 40.00 
-    },   
-    {
-        category: "Museums", 
-        id: "M101", 
-        name: "Bravings Airship Museum", 
-        description: "Enjoy climbing on and in our collection of small airplanes.  You will find bi-planes, experimental planes and small jets.", 
-        location: "101 Airfield Drive",
-        price: 10.00
-    },   
-    {
-        category: "Museums", 
-        id: "M102", 
-        name: "Forks and Spoons Museum", 
-        description: "Enjoy touring our qwerky Forks and Spoons Museum.  It houses the worlds largest collection of, you guessed it, forks and spoons!", 
-        location: "1056 Lost Knives Court",
-        price: 3.00
-    },  
-    {
-        category: "Museums", 
-        id: "M103", 
-        name: "Steenges Computing Museum", 
-        description: "Enjoy our the Stengees Computing Museum that illustrates how computing has changed over the last 60 years.", 
-        location: "103 Technology Way",
-        price: 0.00 
-    },  
-    {
-        category: "Wine Tastings", 
-        id: "WT-101", 
-        name: "Hastings Winery Tours and Tastings", 
-        description: "Hastings Winery is a small, family owned winery in the heart of San Jose, CA. We pride ourselves on producing single vineyard, small-batch, handcrafted premium wines sourced from the finest grapes in our valley.", 
-        location: "10987 FM 1187",
-        price: 12.00 
-    },   
-    {
-        category: "Wine Tastings", 
-        id: "WT-102", 
-        name: "Lone Oak Winery", 
-        description: "We are a family and friend centered winery that thrives to make each of our guests feel right at home. With a growing wine list of the finest local wines, we offer tours and an incredible experience. We are open for to-go, curbside, and delivery.", 
-        location: "121 Burleson Court",
-        price: 0.00 
-    },   
-    {
-        category: "Other", 
-        id: "OTH101", 
-        name: "Fire Department: Ride Along", 
-        description: "Spend the day hanging out at one of our local fire stations, visiting with the staff and learning about their jobs.  If they receive a call, you can ride along with them!", 
-        location: "10 Redline Drive",
-        price: 25.00 
-    },   
-    {
-        category: "Other", 
-        id: "OTH102", 
-        name: "Homes For Our Neighbors", 
-        description: "Yes, you are a visitor!  But what better way to learn about a community than volunteer with the locals to build affordable housing.  Whether it be for an hour or a week, we would love to have you with us!", 
-        location: "Call (555) 555-5555",
-        price: 0.00 
-    }
-];
-// Dropdown for the activities based on the category selected
-let selectActivitiesEl = document.getElementById("select-activities");
-selectActivitiesEl.style.visibility = "visible"; // Unhiding activities selection
-selectActivitiesEl.options.length = 0; // Reseting all options
-
-        let selectOption = document.createElement("option");
-        selectOption.text = "Select one";
+        let selectOption = new Option("Select one");
         selectOption.value = "";
         selectActivitiesEl.appendChild(selectOption);
 
-        // for loop with activities
-            //filter activities by SelectCategoriesEl.value
-            for(let activity of activities){
-                let selectedCategory = selectCategoriesEl.value;
+        let selectedCategory = selectCategoriesEl.value;
 
-                if(activity.category === selectedCategory){
-                    let option = new Option(activity.name);
-                    selectActivitiesEl.appendChild(option);
-
-                    selectActivitiesEl.onchange = function() {
-                        let selectedActivityName = this.value;
-                        let selectedActivity = activities.find(activity => activity.name === selectedActivityName);
-                    
-                        if (selectedActivity) {
-                            // Display the description of the selected activity
-                            let activityDescriptionElement = document.getElementById("activity-details");
-                            activityDescriptionElement.innerHTML = `<p>${selectedActivity.description}</p> 
-                            <p><strong>Location:</strong> ${selectedActivity.location}</p>
-                            <p><strong>Price:</strong> $${selectedActivity.price.toFixed(2)}</p>`;
-
-                            if (selectedActivity.price > 0) {
-                                purchaseForm.style.display = "block"; // Display the form
-                            } else {
-                                purchaseForm.style.display = "none"; // Hide the form
-                            }
-                    };
-                }
+        for (let activity of activities) {
+            if (activity.category === selectedCategory) {
+                let option = new Option(activity.name);
+                selectActivitiesEl.appendChild(option);
             }
-    }
+        }
+
+        selectActivitiesEl.addEventListener("change", function () {
+            let selectedActivityName = this.value;
+            let selectedActivity = getActivityByName(selectedActivityName);
+
+            if (selectedActivity) {
+                displayActivityDetails(selectedActivity);
+            }
+        });
+    };
 };
+
+function getActivityByName(activityName) {
+    for (let i = 0; i < activities.length; i++) {
+        if (activities[i].name === activityName) {
+            return activities[i];
+        }
+    }
+    return null;
+}
+
+function displayActivityDetails(selectedActivity) {
+    let activityDescriptionElement = document.getElementById("activity-details");
+    activityDescriptionElement.innerHTML = 
+    `<p>${selectedActivity.description}</p>
+    <p><strong>Location:</strong> ${selectedActivity.location}</p>
+    <p><strong>Price:</strong> $${selectedActivity.price.toFixed(2)}</p>`;
+
+    if (selectedActivity.price > 0) {
+        purchaseForm.style.display = "block";
+    } else {
+        purchaseForm.style.display = "none";
+    }
+
+    document.getElementById("ticket-btn").addEventListener("click", function () {handleTicketPurchase(selectedActivity);});
+}
+
+// Form to purchase tickets with confirmation message
 function handleTicketPurchase(selectedActivity) {
-    let selectActivitiesEl = document.getElementById("select-activities");
     let numberOfTickets = document.getElementById("numberOfTickets").value;
     let creditCard = document.getElementById("creditCard").value;
     let email = document.getElementById("email").value;
     let confirmationMessage = document.getElementById("confirmationMessage");
 
     if (selectedActivity && numberOfTickets && creditCard && email) {
-        let message = `Your credit card has been charged $${(selectedActivity.price * numberOfTickets).toFixed(2)} for ${numberOfTickets} to ${selectedActivity.name}. A confirmation email has been sent to ${email}.`;
+        let message = `Your credit card has been charged $${(
+            selectedActivity.price * numberOfTickets
+        ).toFixed(2)} for ${numberOfTickets} to ${
+            selectedActivity.name
+        }. A confirmation email has been sent to ${email}.`;
         confirmationMessage.textContent = message;
         confirmationMessage.style.display = "block";
         purchaseForm.style.display = "none";
     }
-};
+}
+
+
 // Dropdown for the categories
 function populateCategoryDropdown() {
-    let categories = ["Select an Option", "Adventures", "Arts & Crafts", "Museums", "Wine Tastings", "Other"];
-    
+    let categories = ["Select an Option","Adventures","Arts & Crafts","Museums","Wine Tastings","Other",];
+
     for (let i = 0; i < categories.length; i++) {
         let categoryName = categories[i];
         let categoryOption = new Option(categoryName);
         selectCategoriesEl.appendChild(categoryOption);
     }
 }
-};
